715fa160119c44c6348e3db3852edd995c3dbfae
Deekshith Naidu <Jayee@Deekshith-8.local>
2024-08-12 16:44:44 -0500

    Commit after a long time without any major changes

---
+++ Engineering_projects/Shear_loading/240712_701_data/240713_neural_ode_701_upon_240712_applying_gpr_with_noise/other_trails_including_finalised/3_kurt_code_ReLU.py
@@ -0,0 +1,263 @@
+import pickle
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.gaussian_process import GaussianProcessRegressor as GPR
+from sklearn.gaussian_process.kernels import RBF,WhiteKernel
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+
+import os
+import argparse
+import time
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+torch.manual_seed(1000)
+np.random.seed(1000)
+
+parser = argparse.ArgumentParser('ODE demo')
+parser.add_argument('--method', type=str, choices=['dopri5', 'adams'], default='dopri5')
+parser.add_argument('--data_size', type=int, default=993)
+parser.add_argument('--batch_time', type=int, default=15)
+parser.add_argument('--batch_size', type=int, default=100)
+parser.add_argument('--niters', type=int, default=2000)
+parser.add_argument('--test_freq', type=int, default=20)
+parser.add_argument('--viz', action='store_true')
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--adjoint', action='store_true')
+
+# Parse arguments when running in Jupyter notebook
+args = parser.parse_args(args=[])
+
+if args.adjoint:
+    from torchdiffeq import odeint_adjoint as odeint
+else:
+    from torchdiffeq import odeint
+
+device = torch.device('cpu')
+
+# Loading the imporant variables file.
+file_name = "important_variables.pkl"
+with open(file_name,"rb") as input_file:
+    imp_vars = pickle.load(input_file)
+
+# Uniform distance data
+tmvsk = imp_vars["UDD"]
+
+# Finding the derivative function as done in the NN case
+## Central difference scheme
+from scipy.interpolate import interp1d
+xTrain = np.concatenate([tmvsk[:,1:],tmvsk[:,0].reshape(-1,1)],1)
+tTrain = tmvsk[:,0]
+gpr_m,gpr_v,gpr_s,gpr_k = [imp_vars["model"][_] for _ in range(4)]
+timeStep = 2e-6
+
+t_plus_dt = xTrain[:,4].reshape(-1,1) + timeStep/2
+t_minus_dt = xTrain[:,4].reshape(-1,1) - timeStep/2
+
+dmeanBydt,dvarBydt,dskwBydt,dkurtBydt = [(imp_vars["model"][_].predict(t_plus_dt)
+                  -imp_vars["model"][_].predict(t_minus_dt))/timeStep 
+                 for _ in range(4)]
+
+dm_dt_fun = interp1d(tTrain, dmeanBydt, axis=0, fill_value='extrapolate')
+dv_dt_fun = interp1d(tTrain, dvarBydt, axis=0, fill_value='extrapolate')
+ds_dt_fun = interp1d(tTrain, dskwBydt, axis=0, fill_value='extrapolate')
+dk_dt_fun = interp1d(tTrain, dkurtBydt, axis=0, fill_value='extrapolate')
+
+
+## Setting up the true system
+true_y0 = torch.from_numpy(xTrain[0,:]).to(device).to(dtype=torch.double)
+t = torch.from_numpy(tTrain).to(dtype=torch.double)
+true_y = torch.from_numpy(xTrain).unsqueeze(1).to(dtype=torch.double)
+
+## Get mini-batch data from the training dataset 
+def get_batch():
+    s = torch.from_numpy(np.random.choice(np.arange(args.data_size - args.batch_time, dtype=np.int64), args.batch_size, replace=False))
+    batch_y0 = true_y[s]  # (M, D)
+    batch_t = torch.stack([t[i:i+args.batch_time] for i in s]) # (T)
+    batch_y = torch.stack([true_y[s + i] for i in range(args.batch_time)], dim=1)  # (T, M, D)
+    batch_y=batch_y.permute(2,0,1,3)
+    return batch_y0.to(device), batch_t.to(device), batch_y.to(device)
+## Construct a neural network to approximate the dynamics of an ODE
+class ODEFuncKurtosis(nn.Module):
+
+    def __init__(self):
+        super(ODEFuncKurtosis, self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(5, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        ).to(torch.float64)
+
+        for m in self.net.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0, std=0.1)
+                nn.init.constant_(m.bias, val=0)
+
+    def forward(self, t, y):
+        return self.net(y)
+    
+funcKurtosis = ODEFuncKurtosis().to(device)
+    
+def combined_system(tVar, y):
+    
+    dt_dt = 1.0
+    dt_dt = torch.tensor([dt_dt])
+    
+    dm_dt =  dm_dt_fun(tVar.item()).item()
+    dm_dt = torch.tensor([dm_dt]).to(dtype=torch.double)
+    
+    dv_dt = dv_dt_fun(tVar.item()).item()
+    dv_dt = torch.tensor([dv_dt]).to(dtype=torch.double)
+   
+    ds_dt = ds_dt_fun(tVar.item()).item()
+    ds_dt = torch.tensor([ds_dt]).to(dtype=torch.double)
+    
+    dk_dt = funcKurtosis(tVar,y)
+    
+    return torch.cat([dm_dt, dv_dt, ds_dt, dk_dt, dt_dt], dim=0)
+
+ii = 0
+## Setting up the optimizer
+
+# Setting up the optimizer with all parameters
+optimizer = optim.RMSprop(funcKurtosis.parameters(), lr=0.005)
+
+for itr in range(1, 1000):
+#for itr in range(1, 20):
+        ## Using mini-batch data to update the neural network coefficients
+        optimizer.zero_grad()
+        batch_y0, batch_t, batch_y = get_batch()
+        solutions = []
+        temp1 = 0
+        for ic in batch_y0:
+            sol = odeint(combined_system, ic[0], batch_t[temp1])
+            solutions.append(sol)
+            temp1 = temp1+1
+        pred_toCalcLoss= torch.stack(solutions)[:,:,3].unsqueeze(0)
+        actual_toCalcLoss = batch_y[:,:,:,3]
+        #pred_y = pred_y.permute(2, 1, 0, 3)
+        loss = torch.mean(torch.abs(pred_toCalcLoss - actual_toCalcLoss))
+        loss.backward()
+        optimizer.step()
+
+        if itr % args.test_freq == 0 or itr == 1:
+            with torch.no_grad():
+                pred_y = odeint(combined_system, true_y0, t)
+                pred_toDisplay = pred_y[:,3].unsqueeze(1)
+                actual_toDisplay = true_y[:,:,3]
+                loss = torch.mean(torch.abs(pred_toDisplay - actual_toDisplay))
+                print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
+                ii += 1
+                
+# Setting up the optimizer with all parameters
+optimizer = optim.RMSprop(funcKurtosis.parameters(), lr=0.0005)
+
+for itr in range(1020, 2000):
+#for itr in range(1, 20):
+        ## Using mini-batch data to update the neural network coefficients
+        optimizer.zero_grad()
+        batch_y0, batch_t, batch_y = get_batch()
+        solutions = []
+        temp1 = 0
+        for ic in batch_y0:
+            sol = odeint(combined_system, ic[0], batch_t[temp1])
+            solutions.append(sol)
+            temp1 = temp1+1
+        pred_toCalcLoss= torch.stack(solutions)[:,:,3].unsqueeze(0)
+        actual_toCalcLoss = batch_y[:,:,:,3]
+        #pred_y = pred_y.permute(2, 1, 0, 3)
+        loss = torch.mean(torch.abs(pred_toCalcLoss - actual_toCalcLoss))
+        loss.backward()
+        optimizer.step()
+
+        if itr % args.test_freq == 0 or itr == 1:
+            with torch.no_grad():
+                pred_y = odeint(combined_system, true_y0, t)
+                pred_toDisplay = pred_y[:,3].unsqueeze(1)
+                actual_toDisplay = true_y[:,:,3]
+                loss = torch.mean(torch.abs(pred_toDisplay - actual_toDisplay))
+                print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
+                ii += 1
+                
+# Setting up the optimizer with all parameters
+optimizer = optim.RMSprop(funcKurtosis.parameters(), lr=0.0001)
+
+for itr in range(2020, 2500):
+#for itr in range(1, 20):
+        ## Using mini-batch data to update the neural network coefficients
+        optimizer.zero_grad()
+        batch_y0, batch_t, batch_y = get_batch()
+        solutions = []
+        temp1 = 0
+        for ic in batch_y0:
+            sol = odeint(combined_system, ic[0], batch_t[temp1])
+            solutions.append(sol)
+            temp1 = temp1+1
+        pred_toCalcLoss= torch.stack(solutions)[:,:,3].unsqueeze(0)
+        actual_toCalcLoss = batch_y[:,:,:,3]
+        #pred_y = pred_y.permute(2, 1, 0, 3)
+        loss = torch.mean(torch.abs(pred_toCalcLoss - actual_toCalcLoss))
+        loss.backward()
+        optimizer.step()
+
+        if itr % args.test_freq == 0 or itr == 1:
+            with torch.no_grad():
+                pred_y = odeint(combined_system, true_y0, t)
+                pred_toDisplay = pred_y[:,3].unsqueeze(1)
+                actual_toDisplay = true_y[:,:,3]
+                loss = torch.mean(torch.abs(pred_toDisplay - actual_toDisplay))
+                print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
+                ii += 1
+
+modelVar = funcKurtosis
+modelVar.eval()
+
+y0 = true_y0
+# Time point for prediction
+t_value = t
+with torch.no_grad():
+    predicted_states = odeint(combined_system,y0 , t).to(device)
+    
+predicted_states=predicted_states.numpy()
+
+fig, ax = plt.subplots(figsize=(10, 8))
+plt.plot(tTrain,xTrain[:,0], 'r',label='mean exp NN interpolated')
+plt.plot(tTrain,predicted_states[:,0], 'r--',label='mean NODE')
+
+plt.plot(tTrain,xTrain[:,1], 'g',label='Kurtosis exp NN interpolated')
+plt.plot(tTrain,predicted_states[:,1], 'g--',label='Kurtosis NODE')
+
+plt.plot(tTrain,xTrain[:,2], 'b',label='skewness exp NN interpolated')
+plt.plot(tTrain,predicted_states[:,2], 'b--',label='skewness NODE')
+
+plt.plot(tTrain,xTrain[:,3], 'k',label='Kurtosis exp NN interpolated')
+plt.plot(tTrain,predicted_states[:,3], 'k--',label='Kurtosis NODE')
+
+ax.legend()
+ax.grid(True, linestyle='--', alpha=0.6)
+
+# Customize fonts and sizes
+ax.set_title("Learning evolution Kurt", fontsize=18, fontweight='bold')
+ax.set_xlabel("X-axis", fontsize=14, fontweight='bold')
+ax.set_ylabel("Y-axis", fontsize=14, fontweight='bold')
+
+# Increase tick label font size
+ax.tick_params(axis='both', labelsize=12)
+
+# Customize the appearance of the legend
+legend = ax.legend(loc='lower right', fontsize=9)
+legend.get_frame().set_edgecolor('black')
+ax.set_facecolor('#F4F4F4')
+
+plt.savefig('learning_3_kurt_ReLU.pdf', bbox_inches='tight')
+
+torch.save(funcKurtosis, '3_kurtosis_ReLU.pth')
+# Show the plot
+# plt.show()
